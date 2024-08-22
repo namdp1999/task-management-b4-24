7dd330bde33fe60906ee78389fabba1553137f0d
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-08-22 22:34:14 +0700

    6. Cấu hình project theo mô hình MVC

--- index.js
+++ index.js
@@ -2,37 +2,15 @@ const express = require("express");
 require('dotenv').config();
 
 const database = require("./config/database");
+
+const routesApi = require("./routes/client/index.route");
+
 database.connect();
 
 const app = express();
 const port = process.env.PORT;
 
-const Task = require("./models/task.model");
-
-app.get("/tasks", async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false
-  });
-
-  res.json(tasks);
-});
-
-app.get("/tasks/detail/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const task = await Task.findOne({
-      _id: id,
-      deleted: false
-    });
-  
-    res.json(task);
-  } catch (error) {
-    res.json({
-      message: "Not Found"
-    });
-  }
-});
+routesApi(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
