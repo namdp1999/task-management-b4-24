3308c0ee693c1dcf5b9f7c78e3ad07296e8e85ba
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-08-24 22:14:23 +0700

    5. Thêm mới công việc

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -94,3 +94,20 @@ module.exports.changeStatus = async (req, res) => {
     });
   }
 }
+
+// [PATCH] /tasks/create
+module.exports.create = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+  
+    res.json({
+      message: "Tạo công việc thành công!",
+      task: task
+    });
+  } catch (error) {
+    res.json({
+      message: "Not Found"
+    });
+  }
+}
