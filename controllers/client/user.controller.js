0e9420eaed6158921027aa1dd53669d431e82d2f
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-08-27 21:48:58 +0700

    2.2. Màn hình nhập mã OTP xác thực

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -119,3 +119,34 @@ module.exports.forgotPassword = async (req, res) => {
     message: "Đã gửi mã OTP qua email!"
   });
 }
+
+// [POST] /users/password/otp
+module.exports.otpPassword = async (req, res) => {
+  const email = req.body.email;
+  const otp = req.body.otp;
+  
+  const result = await ForgotPassword.findOne({
+    email: email,
+    otp: otp
+  });
+
+  if(!result) {
+    res.json({
+      code: 200,
+      message: "OTP không hợp lệ!"
+    });
+    return;
+  }
+
+  const user = await User.findOne({
+    email: email
+  });
+
+  console.log(user);
+
+  res.json({
+    code: 200,
+    message: "Xác thực thành công!",
+    token: user.token
+  });
+}
