ad705e2f76ff339bad23c907526055e018de0faa
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-08-27 21:23:16 +0700

    1. Tính năng đăng nhập

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -42,3 +42,38 @@ module.exports.register = async (req, res) => {
     });
   }
 }
+
+// [POST] /users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!user) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại!"
+    });
+    return;
+  }
+
+  if(md5(password) != user.password) {
+    res.json({
+      code: 400,
+      message: "Sai mật khẩu!"
+    });
+    return;
+  }
+
+  console.log(user);
+
+  res.json({
+    code: 200,
+    message: "Đăng nhập thành công!",
+    token: user.token
+  });
+}
